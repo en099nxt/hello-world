b0fec93a433fb8adf7aa7425c5d7c62d6f06e6fe
en099nxt <en099nxt@gmail.com>
2017-07-14 11:34:11 +0800

    Update netwater.js

--- netwater.js
+++ netwater.js
@@ -364,7 +364,7 @@ if(!__Product_Page_NW__){													//如果不是商品頁
 		$('#M_img').ready(function(){											//大圖
 			$('#M_img').css({'border-radius':'','margin-right':'10px','box-shadow':'0 4px 8px 0 rgba(0,0,0,0.2),0 6px 20px 0 rgba(0,0,0,0.19)'});
 		});
-		$('#bg_pic').ready(function(){											//黑背景
+		$('.imgr8').click(function(){											//黑背景
 			$('#bg_pic').css('opacity','0.3');
 		});
 
