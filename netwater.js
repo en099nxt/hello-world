6dc2bda0a7cdee1d8c204ff50eeaa33711297b4f
en099nxt <en099nxt@gmail.com>
2017-07-17 10:05:44 +0800

    Update netwater.js

--- netwater.js
+++ netwater.js
@@ -408,8 +408,8 @@ if(document.location.href=='http://netwater.shop2000.com.tw/member' || document.
 	$('.right_td .p_tb').css({'background':'#fff','box-shadow':'rgba(51,51,102,0.498039) 0 3px 17px -4px'});		//整體
 	$('.right_td .p_c3b>table tr:nth-of-type(1) div').css({'font-size':'24px','color':'rgb(89,132,202)'});			//電腦版第一區塊標題
 	$('.right_td .p_c3b>div').css({'font-size':'24px','color':'rgb(89,132,202)'});						//電腦版第二區塊標題
-	$('.right_td .p_c3b>table>tbody>tr>td>div').css('border-bottom','0px');							//第一區塊標題下方去虛線
-	$('.right_td .p_c3b>div').css('border-bottom','0px');									//第二區塊標題下方去虛線
+	$('.right_td .p_c3b>table>tbody>tr>td>div').css({'border-bottom':'0px','padding-top':'27px'});				//第一區塊標題下方去虛線、上方加高
+	$('.right_td .p_c3b>div').css({'border-bottom':'0px','padding-top':'27px'}						//第二區塊標題下方去虛線、上方加高
 	$('.table_mem th').css('background-color','#e8f5ff');									//表格標題背景改色
 	$('.table_mem th').css('border','solid 4px #fff');									//表格標題邊框改
 	$('.table_mem td,.table_mem').css('border','0px');									//表格內容邊框去掉
